91cef6b9ec19794a406ae2b714cb89cdc1c83014
SoftwareServices <151872425+pragatss@users.noreply.github.com>
2024-05-26 23:38:46 -0400

    created starting point for oauth login

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BuddyBlazor;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +11,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+Dictionary<string, object> settings = builder.Configuration
+    .GetSection("Local")
+    .Get<Dictionary<string, object>>();
+
+builder.Services.AddOidcAuthentication(options => {
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
 
 await builder.Build().RunAsync();
 
